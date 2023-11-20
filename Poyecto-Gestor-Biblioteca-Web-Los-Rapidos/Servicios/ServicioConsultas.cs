22af7834aa4102c20a32e4332521f46f72b06316
DidacusZ <diegoooooooooooooooo16@gmail.com>
2023-11-20 14:41:14 +0100

    1.0

---
+++ Poyecto-Gestor-Biblioteca-Web-Los-Rapidos/Servicios/ServicioConsultas.cs
@@ -0,0 +1,10 @@
+﻿using DAL.Modelos;
+
+namespace Poyecto_Gestor_Biblioteca_Web_Los_Rapidos.Servicios
+{
+    public interface ServicioConsultas
+    {
+        public void registrarUsuario(Usuarios nuevoUsuario);
+        public bool loginUsuario(string username, string contraseña, string urlApi);
+    }
+}
---
+++ Poyecto-Gestor-Biblioteca-Web-Los-Rapidos/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function revisarContraseña() {
+    var contraseña = document.getElementById('contraseña').value;
+    var confContraseña = document.getElementById('confContraseña').value;
+    var mensajeContraseña = document.getElementById('mensajeContraseña');
+
+    if (contraseña === confContraseña) {
+        mensajeContraseña.textContent = 'Las contraseñas coinciden';
+        mensajeContraseña.style.color = 'green';
+    } else {
+        mensajeContraseña.textContent = 'Las contraseñas no coinciden';
+        mensajeContraseña.style.color = 'red';
+    }
+}
